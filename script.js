61bfd17a8ea3949bf905dceb866986773477adba
treverbrice <treverbrice@gmail.com>
2022-04-25 10:14:40 -0700

    Add operate function

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// add
 function add(num1, num2) {
   return(num1 + num2);
 }
-// subtract
+
 function sub(num1, num2) {
   return(num1 - num2);
 }
-// multiply
+
 function mul(num1, num2) {
   return(num1 * num2);
 }
-// divide
+
 function div(num1, num2) {
   return(num1 / num2);
+}
+
+function operate(num1, operator, num2) {
+  return(operator(num1, num2));
 }
