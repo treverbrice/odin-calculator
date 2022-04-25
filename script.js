978d42fa3197b08c30b13e257456dc829c4555b8
treverbrice <treverbrice@gmail.com>
2022-04-25 10:11:12 -0700

    Add script boilerplate

---
+++ script.js
@@ -0,0 +1,16 @@
+// add
+function add(num1, num2) {
+  return(num1 + num2);
+}
+// subtract
+function sub(num1, num2) {
+  return(num1 - num2);
+}
+// multiply
+function mul(num1, num2) {
+  return(num1 * num2);
+}
+// divide
+function div(num1, num2) {
+  return(num1 / num2);
+}
