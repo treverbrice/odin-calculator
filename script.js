d9ada91c8c1460a78864ef3e43a1b8e6ad45b723
treverbrice <treverbrice@gmail.com>
2022-05-11 12:06:22 -0700

    Add addDigitToDisplay function

--- script.js
+++ script.js
@@ -17,3 +17,21 @@ function div(num1, num2) {
 function operate(num1, operator, num2) {
   return(operator(num1, num2));
 }
+
+function addDigitToDisplay() {
+  const digitToAdd = +this.textContent;
+  const display = document.querySelector("#display");
+  const currentNum = +display.textContent;
+  let newNum = "";
+  // if display is only a zero, replace it
+  if (currentNum != 0) {
+    newNum += currentNum;
+  }
+  newNum += digitToAdd;
+  display.textContent = newNum;
+}
+
+const numButtons = document.querySelectorAll(".numButton");
+numButtons.forEach((numButton) => {
+  numButton.addEventListener("click", addDigitToDisplay);
+});
